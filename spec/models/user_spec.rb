ca268c1569e3104919e914c4d58510d1b01b1cc5
Vadim Makarov <nidred@inbox.ru>
2019-01-12 01:22:30 +0700

    Добавил тесты для модели пользователя и шаблоны для написания тестов модели сообщений и комментариев

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "validation test" do
+    it "ensures default role is user" do
+      test_user = User.new() 
+      expect(test_user.role).to eq("user")  
+    end 
+
+    it "ensures id is present" do
+      test_user = User.new()
+      expect(test_user.valid?).to eq(false)
+    end
+
+    it "ensures email is present" do
+      test_user = User.new(id: 1, created_at: "2019-01-11 07:06:08", updated_at: "2019-01-11 07:06:08")
+      expect(test_user.valid?).to eq(false)
+    end
+    
+  end
+end
